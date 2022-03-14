8d4287b9c808587c4df9a5ed6e3fb15b14ab17ef
madebypixel02 <madebypixel02@gmail.com>
2022-03-14 14:31:31 +0100

    Added initial implementation of megaphone
    Details:
    - Added .gitignore

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aperez-b <aperez-b@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/14 14:12:05 by aperez-b          #+#    #+#             */
+/*   Updated: 2022/03/14 14:29:39 by aperez-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	c;
+
+	j = -1;
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	while (argc > 1 && argv && argv[++j])
+	{
+		i = -1;
+		while (j > 0 && argv[j][++i])
+		{
+			c = std::toupper(argv[j][i]);
+			std::cout << c;
+		}
+	}
+	if (argc > 1 && argv)
+		std::cout << std::endl;
+	return (0);
+}
