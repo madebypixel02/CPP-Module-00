ed63e2ea28e77ad6afd705399c6f329e713074b8
madebypixel02 <madebypixel02@gmail.com>
2022-05-09 18:16:08 +0200

    [WIP] Small advances

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,11 +6,12 @@
 /*   By: aperez-b <aperez-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 11:20:53 by aperez-b          #+#    #+#             */
-/*   Updated: 2022/04/21 12:17:17 by aperez-b         ###   ########.fr       */
+/*   Updated: 2022/05/09 17:33:51 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream>
 
 int	Account::getNbAccounts(void)
 {
@@ -30,4 +31,33 @@ int	Account::getNbDeposits(void)
 int	Account::getNbWithdrawals(void)
 {
 	return (Account::_totalNbWithdrawals);
+}
+
+int	Account::_nbAccounts = 0;
+
+int	Account::_totalNbWithdrawals = 0;
+
+int	Account::_totalNbDeposits = 0;
+
+int	Account::_totalAmount = 0;
+
+Account::Account(int initial_deposit)
+{
+	this->_accountIndex = this->getNbAccounts();
+	this->_amount = initial_deposit;
+	Account::_displayTimestamp();
+	Account::_nbAccounts++;
+	Account::_totalAmount += initial_deposit;
+
+}
+
+void	Account::_displayTimestamp(void)
+{
+	std::cout << "[20220509_172132] ";
+}
+
+void	Account::displayAccountsInfos(void)
+{
+	Account::_displayTimestamp();
+	
 }
